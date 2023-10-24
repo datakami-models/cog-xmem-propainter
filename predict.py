c01ad83887691b3dbd5813559b0ccd37b89ee547
Judith van Stegeren <judith@replicate.com>
2023-10-24 13:21:31 +0000

    Clean up code, config and extend ignore files

--- predict.py
+++ predict.py
@@ -7,6 +7,7 @@ from cog import BasePredictor, Input, Path
 import propainter.predict as propainter
 import xmem.predict as xmem
 
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
@@ -25,7 +26,9 @@ class Predictor(BasePredictor):
         return_intermediate_outputs: bool = Input(description="Return the intermediate processing results in the output.", default=True),
         fp16: bool = Input(description="Use half-precision (fp16), instead of full precision. This speeds up results.", default=True),
     ) -> List[Path]:
-        # note: when we call Predictor.predict() from outside cog, the default values for arguments are not passed to the method.
+        
+        # note: when we call Predictor.predict() from outside cog, 
+        # the default values for arguments are not passed to the method.
         xmem_output = self.xmem_predictor.predict(video=video, mask=mask)
         propainter_outputs = self.propainter_predictor.predict(
             video=video, 
@@ -49,26 +52,4 @@ class Predictor(BasePredictor):
         if return_intermediate_outputs:
             return [xmem_output] + propainter_outputs
         return propainter_outputs
-        # processed_input = preprocess(image)
-        # output = self.model(processed_image, scale)
-        # return postprocess(output)
-
-
-# self,
-#         video: Path = Input(description="Input video"),
-#         mask: Path = Input(description="Mask for video inpainting. Can be a static image (jpg, png) or a video (avi, mp4).", default=None),
-#         return_input_video: bool = Input(description="Return the input video in the output.", default=False),
-#         resize_ratio: float = Input(description="Resize scale for processing video.", default=1.0),
-#         height: int = Input(description="Height of the processing video.", default=-1),
-#         width: int = Input(description="Width of the processing video.", default=-1),
-#         mask_dilation: int = Input(description="Mask dilation for video and flow masking.", default=4),
-#         ref_stride: int = Input(description="Stride of global reference frames.", default=10),
-#         neighbor_length: int = Input(description="Length of local neighboring frames.", default=10),
-#         subvideo_length: int = Input(description="Length of sub-video for long video inference.", default=80),
-#         raft_iter: int = Input(description="Iterations for RAFT inference.", default=20),
-#         mode: str = Input(description="Modes: video inpainting / video outpainting. If you want to do video inpainting, you need a mask. For video outpainting, you need to set scale_h and scale_w, and mask is ignored.", choices=['video_inpainting', 'video_outpainting'], default='video_inpainting'),
-#         scale_h: float = Input(description="Outpainting scale of height for video_outpainting mode.", default=1.0),
-#         scale_w: float = Input(description="Outpainting scale of width for video_outpainting mode.", default=1.0),
-#         save_fps: int = Input(description="Frames per second.", default=24),
-#         fp16: bool = Input(description="Use fp16 (half precision) during inference. Default: fp32 (single precision).", default=False)
 
